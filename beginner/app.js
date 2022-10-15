43e063eefc2d82cb4f3fe3370c05a844c715cd55
GbatZ2 <carloszaragozabeato15@gmail.com>
2022-10-15 18:52:12 +0200

    finished

---
+++ beginner/app.js
@@ -0,0 +1,131 @@
+
+//First Lesson
+//console.log("First Practice")
+//console.info("First Practice")
+//console.error("First Practice")
+//console.warn("First Practice");
+//alert("First Practice")
+
+//Second Lesson
+//var message1 = "Second Lesson"
+//let message2 = "Second Lesson"
+//const MESSAGE3 = "Second Lesson"
+//console.log(message1);
+//console.log(message2);
+//console.log(MESSAGE3);
+
+//Third Lesson
+//let result = (884/541)+50
+//let result2 = 52>10
+//let result3 = 11 >= 10
+//let  result4 = 50 != 50
+//alert(result4)
+//console.log(typeof(result4));
+//console.log(result2.toString());
+//const notNumber = 2+"a";
+//console.log(isNaN(notNumber));
+//console.log(result.toFixed())
+
+
+//Number Practice
+
+//const TOTAL = 102
+//const PEOPLE = 6
+//const TOTAL_BY_PERSON = TOTAL/PERSON
+//alert(TOTAL_BY_PERSON)
+
+//const PHONE = 300
+//const HEADPHONES = 30
+//const CASE = 10
+//const DISCOUNT = 0.1
+//const RESULT = (PHONE + HEADPHONES + CASE) * DISCOUNT
+//alert(RESULT)
+
+//Fourth Lesson
+//let user = "Carlos";
+
+//console.log("HELLO " + "WORLD");
+//console.log(`Hello ${user}`);
+//console.log(typeof(user));
+//console.log(user.length);
+//console.log(user.includes("Ca"));
+//console.log(user.slice[1,3]);
+//console.log(user.replace("a","e"));
+//console.log(user.trim());
+//console.log(user.split("a"));
+
+//Practice Strings
+
+//const NAME = "Carlos"
+//const GAME = "Link awakening"
+//const DISCOUNT = "20%"
+
+//const TEXT = `Hola ${NAME} el juego es ${GAME}, y tiene un descuento de ${DISCOUNT}`
+//console.log(text);
+
+
+//Fifth Lesson
+//console.log(Boolean(1));
+//console.log(Boolean('WhatEver'));
+//console.log(Boolean(3.14));
+//console.log(Boolean(100===5));
+//console.log(Boolean(4<4));
+//console.log(Boolean("1" === 1));
+
+
+//Practice Booleans
+//let user_logged = false
+//const BLOCK_TEXT = ""=="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+//if(BLOCK_TEXT){
+//    user_logged = true
+//}else{
+//  user_logged = false
+//}
+
+//Sixth Lesson
+//let a
+//console.log(a);
+//a = "text"
+//console.log(a);
+//let b = null
+//console.log(b);
+
+//Seventh Lesson
+
+//function sayHello(){
+//  console.log("HELLO")}
+//function sum(value1, value2){
+//  let result = value1 + value2
+//  console.log(`The result is ${result}`);}
+//function substract(value1, value2){
+//  let result = value1 - value2
+//  return `The result is ${result}`}
+
+//Eight Lesson
+//let say_hello = function(name = "World"){
+//  return `Hello ${name}`
+//}
+
+//const message = 'Text example'
+//let say_message = (name) => `Hello ${name}`
+//say_message(message)
+
+//Practice Exercise
+//const flight = 100
+//const hotel = 50
+//const tour = 30
+//const budget = 300
+//const  total = flight + hotel + tour
+//if(budget > total){
+//    console.log("Yeah, i am a rich booy");
+//}else if(budget == total){
+//    console.log("Yes, but you dont have much more money to spend");
+//}else {
+//  console.log("No, i dont have enought money");
+//}
+//const flight = 100
+//const hotel = 50
+//const tour = 30
+//const  total = flight + hotel + tour
+//const budget = 300
+//const result = tour < budget ?console.log("Yes") : console.log("No");
