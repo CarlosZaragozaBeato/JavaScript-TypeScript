088417f1de892c3505e5bf3aefc1b36ca13b121a
GbatZ2 <carloszaragozabeato15@gmail.com>
2022-10-19 06:36:04 +0200

    finished

---
+++ app.js
@@ -0,0 +1,73 @@
+///////////
+//Objetos//
+///////////
+/*const persona1 ={
+  nombre: "Carlos",
+  edad: 30,
+  saludar: ()=>{
+    console.log(`HOLA ${this.nombre}`);
+  }
+};
+function Persona(nombre,edad){
+  this.nombre = nombre;
+  this.edad = edad;
+
+  this.saludar = ()=>{
+    console.log(`HOLA ${this.nombre}`);
+  }
+};
+var persona = new Persona("Carlos", 23);
+persona.saludar()
+console.log(persona);
+console.log(persona.edad)*/
+
+
+//let fuerza = {
+//  fuerza: 1,
+//  incrementarFuerza: function (){
+//    this.fuerza+=1;
+//  },
+//  consultarFuerza: function (){
+//    console.log(this.fuerza);
+//  }
+//}
+//fuerza.consultarFuerza()
+//fuerza.incrementarFuerza()
+//fuerza.consultarFuerza()
+
+//DATE//
+//let date = new Date();
+//let day = date.getDate();
+//let month = date.getMonth()+1;
+//let year = date.getFullYear();
+//let fullDate = `Today is ${day}, the month is ${month} and the year is ${year}`;
+//console.log(fullDate);
+
+//MATH//
+//let pi = Math.PI;
+//console.log(Math.min(1, 2, 3, 4, 5, -10));
+//console.log(Math.max(1, 2, 3, 4, 5, -10));
+//console.log(Math.round(4.5));
+//console.log(Math.floor(4.8));
+//console.log(Math.ceil(4.2));
+//let aleatorio = Math.random();
+//let generarNumero = (max) => {
+//  return Math.round(Math.random() * max);
+//}
+//console.log(generarNumero(100));
+
+//ARRAYS//
+//let colores = ['rojo', 'verde', 'azul'];
+//colores.push('rosa');
+//console.log(colores);
+
+
+//let numeros = [1, 2, 3, 4, 5, 6, 9, 8, 7, 4, 1];
+//let result = 0;
+//for (let i = 0; i < numeros.length; i++){
+//  if (numeros[i] % 2 == 0) {
+//    console.log(numeros[i]);
+//  }
+//  result += numeros[i];
+//}
+//console.log(result);
